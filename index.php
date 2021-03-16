88a1776748259584662907e7b054284cde688432
melvinsibichen <mca529@rajagiri.edu>
2021-03-16 13:52:53 -0700

    carrental created

---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>indexcar</title>
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+    <div class="container-fluid">
+      <img src="photos\logo2.png" height="50" alt="">
+      <a class="navbar-brand" href="#">CAR RENTAL ZONE</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="company.php">CARS AVAILABLE</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="booking.php">YOUR BOOKING</a>
+          </li>
+        </ul>
+        
+        <a class="nav-link" href="signup.php" style="color: rgb(8, 241, 8);">SIGN IN</a>
+        <a class="nav-link" href="signin.php" style="color: rgb(38, 247, 38);">LOG IN</a>
+        
+      </div>
+    </div>
+  </nav>
+<body background="photos\index2.jpg">
+   <div class="container">
+       <div class="row">
+           
+           <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-12 col-xxl-12">
+
+           <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="photos\index11.jpg" height="600" width="900"  class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h3>CAR ZONE COCHIN</h3>
+                  <p>Clean cars. Flexible bookings. Socially distant rental counters.</p>
+                </div>
+              </div>Clean cars. Flexible bookings. Socially distant rental counters.
+              <div class="carousel-item">
+                <img src="photos\index2.jpg" height="600" width="900"  class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h3>CAR ZONE COCHIN</h3>
+                  <p>Clean cars. Flexible bookings. Socially distant rental counters.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="photos\index33.jpg" height="600" width="900" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h3>CAR ZONE COCHIN</h3>
+                  <p>Clean cars. Flexible bookings. Socially distant rental counters.</p>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+       </div>
+   </div>
+
+   <div class="row">
+    <div class="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+        <h4 style="background-color: gray;">TESTIMONIALS</h4>
+        <p style="background-color: gray;">
+            We Specialize in Customer Service. 
+            <br>Our clients enjoy concierge like service from the time they take delivery of their luxury or Exotic car.
+            <br> 
+            Our representatives are experienced with features of these unique cars and aquaint each renter with the amazing features, and special options offered by the car.<br>
+            The convenience starts from the time a client visits our website! Our website is designed to simplify the Luxury or Exotic Car Rental process. <br>
+            To recieve an instant rate quote simply input your pick-up and drop-off locations in the “Get Quote” link above. 
+            <br>Input your dates and times and select your car choice from our selection of luxury and exotic cars .
+            <br> You will instantly be quoted a rate that includes all taxes and fees. 
+            <br>You then have the option to select insurance waivers, additional miles, additional delivery and pickup services etc. 
+            <br>
+            Our website makes reserving your luxury or exotic car hassle-free.
+        </p>
+    </div>
+    <div class="col col-12 col-sm-6 col-md-6 col-lg-8 col-xl-8 col-xxl-8">
+        <h4 style="background-color: gray;">New Arrival</h4>
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="photos\update1.jpg" height="400" width="700" class="d-block w-55" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="photos\update2.jpg" height="400" width="700" class="d-block w-55" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="photos\update3.jpg" height="400" width="700" class="d-block w-55" alt="...">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+        <br>
+        <br>
+        <h6 style="font-family:verdana;color:rgb(9, 83, 19);"><i>CAR ZONE</i></h6>
+        <table class="table table-borderless">
+            <tr><td><a class="navbar-brand" href=""><i>AUDI</i></a></td></tr>  
+            <tr><td><a class="navbar-brand" href=""><i>BENZ</i></a></td></tr>
+            <tr><td><a class="navbar-brand" href=""><i>BMW</i></a></td></tr>
+            <tr><td><a class="navbar-brand" href=""><i>PORSCHE</i></a></td></tr>
+        </table>
+    </div>
+    <div class="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+        <br>
+        <br>
+        <h6 style="color: aliceblue;"><b><i>USEFUL LINKS</i></b></h6>
+        <table class="table table-borderless">
+            <tr><td><a class="navbar-brand" style="color: aliceblue;" href=""><b><i>COMPANY</b></i></a></td></tr>  
+            <tr><td><a class="navbar-brand" style="color: aliceblue;" href=""><i><b>JAGUAR</b></i></a></td></tr>
+            <tr><td><a class="navbar-brand" style="color: aliceblue;" href=""><i><b>LAMBORGINI</b></i></a></td></tr>
+            <tr><td><a class="navbar-brand" style="color: aliceblue;" href=""><b><i>About Us</b></i></a></td></tr>
+        </table>
+    </div>
+</div>
+</div>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ lamborgini.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lamborgini</title>
+</head>
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+  <div class="container-fluid">
+    <img src="photos\logo2.png" height="50" alt="">
+    <a class="navbar-brand" href="#">CAR RENTAL ZONE</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="company.php">CARS AVAILABLE</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="booking.php">YOUR BOOKING</a>
+        </li>
+      </ul>
+      
+      <a class="nav-link" href="signup.php" style="color: rgb(8, 241, 8);">SIGN IN</a>
+      <a class="nav-link" href="signin.php" style="color: rgb(38, 247, 38);">LOG IN</a>
+      
+    </div>
+  </div>
+</nav>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-3 col-md-4 col-lg-6 col-xl-12 col-xxl-12">
+                <table class="table">
+                    <tr>
+                        <td><div class="card" style="width: 18rem;">
+                            <img src="photos\lambo Reventón.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">REVENTON</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                              <a href="booking.php" class="btn btn-primary">Rent Now</a>
+                            </div>
+                          </div>
+                          </td>
+                        <td><div class="card" style="width: 18rem;">
+                            <img src="photos\lambo SC18 Alston.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">ALSTON</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                              <a href="booking.php" class="btn btn-primary">Rent Now</a>
+                            </div>
+                          </div></td>
+                       
+                        <td><div class="card" style="width: 18rem;">
+                            <img src="photos\Lamborghini_Veneno.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">VENENO</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                              <a href="booking.php" class="btn btn-primary">Rent Now</a>
+                            </div>
+                          </div></td>
+                        </tr>
+                        <tr>
+
+                        <td>
+                            <div class="card" style="width: 18rem;">
+                                <img src="photos\lambo urus.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <h5 class="card-title">URUS</h5>
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                  <a href="booking.php" class="btn btn-primary">Rent Now</a>
+                                </div>
+                              </div>
+                        </td>
+                        <td>
+                            <div class="card" style="width: 18rem;">
+                                <img src="photos\LAMBO AVENTADOR.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <h5 class="card-title">AVENTADOR</h5>
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                  <a href="booking.php" class="btn btn-primary">Rent Now</a>
+                                </div>
+                              </div>
+                        </td>
+                        <td>
+                            <div class="card" style="width: 18rem;">
+                                <img src="photos\lambo huracan.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <h5 class="card-title">HURACAN</h5>
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                  <a href="booking.php" class="btn btn-primary">Rent Now</a>
+                                </div>
+                              </div>
+                        </td>
+                    </tr>
+                </table>
+                
+                  
+                  
+                  
+                  
+                  
+
+
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>   
+</body>
+</html>
